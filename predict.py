c12dce25c7096c70fe325a72a7b96d40c36bbbf3
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2020-03-06 23:58:23 +0200

    try new commit

---
+++ predict.py
@@ -0,0 +1,105 @@
+﻿from SurnameDataset import *
+from SurnameClassifier import *
+from Dataloader import *
+from argparse import Namespace
+import torch
+import torch.optim as optim
+from tqdm import tqdm_notebook
+import os
+import re
+
+PATH = "savedmodel.tar"
+
+
+
+args = Namespace(
+     # Data and Path hyper parameters
+    surname_csv="surnames/surnames_with_splits.csv",
+    vectorizer_file="vectorizer.json",
+    model_state_file="model.pth",
+    save_dir="model_storage/ch6/surname_classification",
+    # Model hyper parameter
+    char_embedding_size=100,
+    rnn_hidden_size=64,
+    # Training hyper parameter
+    num_epochs=100,
+    learning_rate=1e-3,
+    batch_size=64,
+    seed=1337,
+    early_stopping_criteria=5,
+    # Runtime hyper parameter
+    cuda=True,
+    catch_keyboard_interrupt=True,
+    reload_from_files=False,
+    expand_filepaths_to_save_dir=True
+)
+
+
+# Preprocess the reviews
+def preprocess_text(text):
+    text = ' '.join(word.lower() for word in text.split(" "))
+    text = re.sub(r"([.,!?])", r" \1 ", text)
+    text = re.sub(r"[^a-zA-Z.,!?]+", r" ", text)
+    return text
+
+
+def predict_nationality(surname, classifier, vectorizer):
+    vectorized_surname, vec_length = vectorizer.vectorize(surname)
+    vectorized_surname = torch.tensor(vectorized_surname).unsqueeze(dim=0)
+    vec_length = torch.tensor([vec_length], dtype=torch.int64)
+    
+    result = classifier(vectorized_surname, vec_length, apply_softmax=True)
+    probability_values, indices = result.max(dim=1)
+    
+    index = indices.item()
+    prob_value = probability_values.item()
+
+    predicted_nationality = vectorizer.nationality_vocab.lookup_index(index)
+
+    return {'nationality': predicted_nationality, 'probability': prob_value, 'surname': surname}
+
+
+
+if not torch.cuda.is_available():
+    args.cuda = False
+args.device = torch.device("cuda" if args.cuda else "cpu")
+
+# create dataset and vectorizer
+dataset = SurnameDataset.load_dataset_and_make_vectorizer(args.surname_csv)
+vectorizer = dataset.get_vectorizer()
+
+
+classifier = SurnameClassifier(embedding_size=args.char_embedding_size, 
+                               num_embeddings=len(vectorizer.char_vocab),
+                               num_classes=len(vectorizer.nationality_vocab),
+                               rnn_hidden_size=args.rnn_hidden_size,
+                               padding_idx=vectorizer.char_vocab.mask_index)
+
+
+classifier = classifier.to(args.device)
+
+dataset.class_weights = dataset.class_weights.to(args.device)
+    
+loss_fuc = nn.CrossEntropyLoss()
+optimizer = optim.Adam(classifier.parameters(), lr=args.learning_rate)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer,
+                                           mode='min', factor=0.5,
+                                           patience=1)
+
+
+                                           
+if os.path.exists(PATH):
+  checkpoint = torch.load(PATH)
+  classifier.load_state_dict(checkpoint['model_state_dict'])
+  optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+  print("checkpoint loaded")
+  
+  
+  
+  
+#predict
+sample = 'Ammar'
+print(predict_nationality(sample, classifier, vectorizer))
+
+
+
