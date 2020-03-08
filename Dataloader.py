e76f9f6ab507aaae90acb32c66e8114a485f46c8
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2020-03-09 00:02:39 +0200

    dataloader.py added

---
+++ Dataloader.py
@@ -0,0 +1,8 @@
+﻿import torch.utils.data
+def generate_batches(dataset, batch_size, shuffle=True, drop_last=True, device="cpu"):    
+    dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size,shuffle=shuffle, drop_last=drop_last)
+    for data_dict in dataloader:       
+      out_data_dict = {}       
+      for name, tensor in data_dict.items():  
+           out_data_dict[name] = data_dict[name].to(device)   
+      yield out_data_dict
